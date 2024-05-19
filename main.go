321bbc0c61ec9e5ac271f3568b626e3d7bfcf0cb
Ade Indra Pratama <adeprathama@gmail.com>
2024-05-19 19:59:36 +0700

    Add Env feature

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Can't load .env file")
+	}
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("It is working!")
+	})
+
+	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
+
+	app.Listen(port)
 }
